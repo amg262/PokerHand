bdb02d7047a16c028cad1dee6a2ffa71e00a8c2e
Andrew <andrewmgunn26@gmail.com>
2022-04-13 13:37:14 -0500

    a

--- src/edu/wctc/test/TheTest.java
+++ src/edu/wctc/test/TheTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class TheTest {
 
@@ -27,24 +26,38 @@ public class TheTest {
     public void testRoyalFlush() {
         Deck deck = new Deck();
         List<Card> cards = new ArrayList<>();
-        cards.add(new Card(Faces.TWO,Suits.CLUB));
-        cards.add(new Card(Faces.JACK,Suits.CLUB));
-        cards.add(new Card(Faces.QUEEN,Suits.CLUB));
-        cards.add(new Card(Faces.KING,Suits.CLUB));
-        cards.add(new Card(Faces.ACE,Suits.CLUB));
+        cards.add(new Card(Faces.KING, Suits.CLUB));
+
+        cards.add(new Card(Faces.TEN, Suits.CLUB));
+        cards.add(new Card(Faces.JACK, Suits.CLUB));
+        cards.add(new Card(Faces.QUEEN, Suits.CLUB));
+        cards.add(new Card(Faces.ACE, Suits.CLUB));
         Hand hand = new Hand(cards);
         assertTrue(CheckHand.isRoyalFlush(hand));
+    }
+
+    @Test
+    public void testNotRoyalFlush() {
+        Deck deck = new Deck();
+        List<Card> cards = new ArrayList<>();
+        cards.add(new Card(Faces.TWO, Suits.CLUB));
+        cards.add(new Card(Faces.JACK, Suits.CLUB));
+        cards.add(new Card(Faces.QUEEN, Suits.CLUB));
+        cards.add(new Card(Faces.KING, Suits.CLUB));
+        cards.add(new Card(Faces.ACE, Suits.CLUB));
+        Hand hand = new Hand(cards);
+        assertFalse(CheckHand.isRoyalFlush(hand));
     }
 
     @Test
     public void testFlush() {
         Deck deck = new Deck();
         List<Card> cards = new ArrayList<>();
-        cards.add(new Card(Faces.EIGHT,Suits.CLUB));
-        cards.add(new Card(Faces.EIGHT,Suits.CLUB));
-        cards.add(new Card(Faces.EIGHT,Suits.CLUB));
-        cards.add(new Card(Faces.EIGHT,Suits.CLUB));
-        cards.add(new Card(Faces.EIGHT,Suits.CLUB));
+        cards.add(new Card(Faces.EIGHT, Suits.CLUB));
+        cards.add(new Card(Faces.EIGHT, Suits.CLUB));
+        cards.add(new Card(Faces.EIGHT, Suits.CLUB));
+        cards.add(new Card(Faces.EIGHT, Suits.CLUB));
+        cards.add(new Card(Faces.EIGHT, Suits.CLUB));
         Hand hand = new Hand(cards);
         assertTrue(CheckHand.isFlush(hand));
     }
